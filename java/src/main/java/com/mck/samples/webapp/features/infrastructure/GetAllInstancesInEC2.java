b18f6fe04448b9176c424f64021d335de9980744
uzi mamani <uzigula@gmail.com>
2019-04-16 14:03:03 -0500

    fix errors

--- java/src/main/java/com/mck/samples/webapp/features/infrastructure/GetAllInstancesInEC2.java
+++
@@ -1,9 +0,0 @@
-package com.mck.samples.webapp.features.infrastructure;
-
-import com.mck.samples.webapp.mediator.BaseResponse;
-import com.mck.samples.webapp.mediator.IAmCommand;
-
-import java.util.List;
-
-public class GetAllInstancesInEC2 implements IAmCommand<BaseResponse<List<String>>> {
-}
